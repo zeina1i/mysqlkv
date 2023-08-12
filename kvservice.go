721f4326dff34040e0074946080efdb7100625c3
hossein zeinali <hosseinzeinalii@gmail.com>
2023-08-13 00:09:25 +0200

    Fix gitignore and some small other fixes

--- kvservice.go
+++ kvservice.go
@@ -1,9 +1,5 @@
 package mysqlkv
 
-import (
-	"log"
-)
-
 type KVService struct {
 	store Store
 }
@@ -21,7 +17,6 @@ func (s *KVService) Put(k string, v string) error {
 func (s *KVService) Get(k string) (string, error) {
 	k, err := s.store.getKV(k)
 	if err != nil {
-		//log.Fatal(err)
 		return "", err
 	}
 
@@ -30,20 +25,12 @@ func (s *KVService) Get(k string) (string, error) {
 
 func (s *KVService) Del(k string) error {
 	err := s.store.updateKVExpiry(k, -1)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
 
-	return nil
+	return err
 }
 
 func (s *KVService) TTL(k string, expiry int64) error {
 	err := s.store.updateKVExpiry(k, expiry)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
 
 	return err
 }
